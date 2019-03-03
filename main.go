54a4b8d46dc8396a617925f93c63f7134e8fe28f
k-nasa <htilcs1115@gmail.com>
2019-03-03 16:59:54 +0900

    add entry points

--- main.go
+++ main.go
@@ -6,10 +6,40 @@ import (
 	"github.com/labstack/echo"
 )
 
+func postUsers(c echo.Context) error {
+  return nil
+}
+
+func postAuthenticate(c echo.Context) error {
+  return nil
+}
+
+func getChannels(c echo.Context) error {
+  return nil
+}
+
+func getMessages(c echo.Context) error {
+  return nil
+}
+
+func postAddChannel(c echo.Context) error {
+  return nil
+}
+
+func postAddMessage(c echo.Context) error {
+  return nil
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+  e.GET("/api/channels", getChannels)
+  e.GET("/api/messages", getMessages)
+  e.POST("/api/users", postUsers)
+  e.POST("/api/authenticate", postAuthenticate)
+  e.POST("/api/channels", postAddChannel)
+  e.POST("/api/messages", postAddMessage)
+	e.Logger.Fatal(e.Start(":8000"))
 }
